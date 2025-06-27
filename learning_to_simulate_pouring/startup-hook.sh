160e0a6d8a89ef8120419c8992dbb68b40c89f7a
Carola Stork <carola.stork@stud.tu-darmstadt.de>
2025-06-27 17:38:12 +0200

    first attempt at getting the 42 stuff to run

--- learning_to_simulate_pouring/startup-hook.sh
+++ learning_to_simulate_pouring/startup-hook.sh
@@ -6,6 +6,10 @@ if command -v conda &> /dev/null; then
 
     # Do that for every new interactive terminal session
     echo 'eval "$(conda shell.bash hook)" && conda activate myenv' >> ~/.bashrc
+
+    # Install required Python packages
+    pip gymnasium==1.1.1
+    pip install /server/pouring_env/ 
 else
     echo "Conda is not installed in this environment."
 fi
